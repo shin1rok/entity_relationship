97729335b383617391a6f01171f29bebb24b44f4
shin1rok <skssikm@gmail.com>
2018-11-16 08:42:23 +0900

    total_price, delivery_price追加

--- db/migrate/20181113234526_create_product_prices.rb
+++ db/migrate/20181113234526_create_product_prices.rb
@@ -2,7 +2,7 @@ class CreateProductPrices < ActiveRecord::Migration[5.2]
   def change
     create_table :product_prices do |t|
       t.decimal :price
-      t.timestamps
+      t.datetime :created_at, comment 'Updateすると購入時点の値段が算出できなくなるので、InsertとDeleteのみにする'
     end
   end
 end
