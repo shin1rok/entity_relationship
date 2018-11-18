bb080c367ae2b276ead4548cdb8858047f86bd38
shin1rok <skssikm@gmail.com>
2018-11-18 16:02:07 +0900

    has_and_belongs_to_many関連付けに対応する結合テーブルについて
    https://railsguides.jp/association_basics.html#has-and-belongs-to-many%E9%96%A2%E9%80%A3%E4%BB%98%E3%81%91%E3%81%AB%E5%AF%BE%E5%BF%9C%E3%81%99%E3%82%8B%E7%B5%90%E5%90%88%E3%83%86%E3%83%BC%E3%83%96%E3%83%AB%E3%82%92%E4%BD%9C%E6%88%90%E3%81%99%E3%82%8B

--- db/migrate/20181117012233_products_suppliers.rb
+++ db/migrate/20181117012233_products_suppliers.rb
@@ -4,5 +4,11 @@ class ProductsSuppliers < ActiveRecord::Migration[5.2]
       t.belongs_to :product, index: true
       t.belongs_to :supplier, index: true
     end
+
+    # この書き方でも↑と同じ
+    # create_join_table :products, :suppliers do |t|
+    #   t.index :product_id
+    #   t.index :supplier_id
+    # end
   end
 end
